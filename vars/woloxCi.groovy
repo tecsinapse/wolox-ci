da2f89f2f1414ecda47885d35590882008a57908
Rodrigo Silva <rbsilva.ti@gmail.com>
2019-08-16 12:10:08 -0300

    fix redundant library annotation

--- vars/woloxCi.groovy
+++ vars/woloxCi.groovy
@@ -1,3 +1,4 @@
+@Library('tec-wolox-ci')
 import com.wolox.parser.ConfigParser;
 import com.wolox.*;
 
@@ -7,12 +8,10 @@ def call(String yamlName) {
     def buildNumber = Integer.parseInt(env.BUILD_ID)
 
     // load project's configuration
-    ProjectConfiguration projectConfig = ConfigParser.parse(yaml, env);
-
-    def imageName = projectConfig.dockerConfiguration.imageName().toLowerCase();
+    ProjectConfiguration projectConfig = ConfigParser.parse(yaml, buildNumber);
 
     // build the image specified in the configuration
-    def customImage = docker.build(imageName, "--file ${projectConfig.dockerfile} .");
+    def customImage = docker.build("${projectConfig.projectName}:${env.BUILD_ID}", "--file ${projectConfig.dockerfile} .");
 
     // adds the last step of the build.
     def closure = buildSteps(projectConfig, customImage);
@@ -20,13 +19,30 @@ def call(String yamlName) {
     // each service is a closure that when called it executes its logic and then calls a closure, the next step.
     projectConfig.services.each {
 
-        closure = "${it.service.getVar()}"(projectConfig, it.version, closure);
+        closure = "${it.getVar()}"(projectConfig, closure);
     }
 
     // we execute the top level closure so that the cascade starts.
     try {
         closure([:]);
     } finally{
-        deleteDockerImages(projectConfig);
+        try {
+            def firstImage = sh(
+                script: "docker images --filter 'reference=${projectConfig.projectName}:*' --format \"{{.Tag}}\" | sort -n | head -1",
+                returnStdout: true
+            );
+            firstImage = Integer.parseInt(firstImage.trim());
+            println firstImage
+            for(int i = firstImage; i < buildNumber; i++) {
+                try {
+                    sh "docker images --filter 'reference=${projectConfig.projectName}:${i}' -q | xargs --no-run-if-empty docker rmi -f"
+                } catch(ignored) {
+                    println ignored
+                }
+            }
+        } catch(ignored) {
+            println ignored
+            //we don't fail for this exception
+        }
     }
 }
